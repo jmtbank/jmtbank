f0bcc9428565e371b4852d3749f281580f4dd684
Gertjan Kamphuis <g.kamphuis@student.utwente.nl>
2011-05-25 12:58:45 +0200

    Added TransactionServer (proper package)

--- src/bank/server/TransactionServer.java
+++ src/bank/server/TransactionServer.java
@@ -1,9 +1,11 @@
-package bank.banking;
+package bank.server;
 
 import java.rmi.*;
 import bank.Account;
 import bank.access.DataAccess;
 import bank.access.DataAccessException;
+import bank.banking.Transaction;
+import bank.banking.Transactor;
 
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -11,37 +13,33 @@ import java.rmi.server.UnicastRemoteObject;
 import java.rmi.RemoteException;
 
 public class TransactionServer {
+	public static final String RMI_NAME = "bank.banking.Transaction";
 
 	public static void main (String[] args) {
-		
 		Remote db;
 
 		if (System.getSecurityManager() == null) {
+			System.out.println("SecurityManager null, creating new SecurityManager");
 			System.setSecurityManager(new SecurityManager());
 		}
-
+		String loc = "localhost";
+		if(args.length == 1) {
+			loc = args[0];
+		}
 		try {
-			//B{DataAccess.lookup}
-			Registry remoteregistry = LocateRegistry.getRegistry(args[0]);
-			db = remoteregistry.lookup(args[1]);
-			//E{DataAccess.lookup}
+			Registry remoteregistry = LocateRegistry.getRegistry(loc);
+			db = remoteregistry.lookup(DataAccessServer.RMI_NAME);
 		} catch (Exception re) {
-			System.err.println(
-			"Registry exception in TransactionServer.main: " + re);
+			System.err.println("Registry exception in TransactionServer.main: " + re);
 			return;
 		}
 		
 		try {
-        //B{Transaction.stub}
         	Transactor trans = new Transactor((DataAccess)db);
         	Transaction stub =
-                (Transaction) UnicastRemoteObject.exportObject(trans, 0);
-        //E{Transaction.stub}
-        //B{Transaction.register}  
-            String name = "Transaction";
+                (Transaction) UnicastRemoteObject.exportObject(trans, 0);  
             Registry localregistry = LocateRegistry.getRegistry();
-            localregistry.rebind(name, stub);
-        //E{Transaction.register}
+            localregistry.rebind(RMI_NAME, stub);
             System.out.println("Transaction Server ready.");
         } catch (RemoteException re) {
             System.err.println("Exception in TransactionServer.main: " + re);
